45af89801c57962a8ed5a2796ff67180caaa2c5d
Michal Josef Špaček <tupinek@gmail.com>
2018-02-07 07:47:24 +0100

    Prepare for 0.07 version

--- Set.pm
+++ Set.pm
@@ -6,7 +6,7 @@ use warnings;
 use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -163,6 +163,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
