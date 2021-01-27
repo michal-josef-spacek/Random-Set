fd01be6fb662bb5f9e4b115462f4fc0408cf1a12
Michal Josef Špaček <tupinek@gmail.com>
2021-01-27 19:29:41 +0100

    Prepare for 0.08 version

--- Set.pm
+++ Set.pm
@@ -6,7 +6,7 @@ use warnings;
 use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 # Constructor.
 sub new {
@@ -168,6 +168,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
