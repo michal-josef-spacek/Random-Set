d42e1bdb99d7a0fae87577e29277c60bf89f2911
Michal Josef Špaček <tupinek@gmail.com>
2021-01-27 19:24:44 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- Set.pm
+++ Set.pm
@@ -158,8 +158,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © Michal Josef Špaček 2013-2021
- BSD 2-Clause License
+© Michal Josef Špaček 2013-2021
+
+BSD 2-Clause License
 
 =head1 VERSION
 
